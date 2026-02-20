5fa351692b64d0979a0d678f1258827511794bad
Janice V. Hernandez <janice.hernandez@neu.edu.ph>
2026-02-20 14:10:06 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9238746206-cdb3f",
+  "appId": "1:1053356713024:web:c4e3ccb7541587f4c22ba5",
+  "apiKey": "AIzaSyAtd4wIEfLJU0wrOQF70IN1MK6Hjc9OHMU",
+  "authDomain": "studio-9238746206-cdb3f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1053356713024"
+};
